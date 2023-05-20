96a29a9f1b4fd2a2be196224ba938c55b2d81d11
KorArty24 <48129496+KorArty24@users.noreply.github.com>
2023-05-20 22:45:51 +0300

    initial commit

---
+++ WpfControlsAndAPIs/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControlsAndAPIs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.myInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            this.inkRadio.IsChecked = true;
+            this.comboColors.SelectedIndex = 0;
+            PopulateDocument();
+        }
+        private void RadioButtonClicked(object sender, RoutedEventArgs e)
+        {
+            switch((sender as RadioButton)?.Content.ToString())
+            {
+                case "Ink Mode!":
+                    this.myInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                    break;
+                case "Erase Mode!":
+                    this.myInkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+                    break;
+                case "Select Mode!":
+                    this.myInkCanvas.EditingMode = InkCanvasEditingMode.Select;
+                    break;
+            }
+        }
+
+        private void ColorChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string colorToUse = (this.comboColors.SelectedItem as StackPanel).Tag.ToString();
+            this.myInkCanvas.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString(colorToUse);
+        }
+
+        private void SaveData(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("StrokeData.bin", FileMode.Create))
+            {
+                this.myInkCanvas.Strokes.Save(fs);
+                fs.Close();
+            }
+        }
+
+        private void LoadData(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("StrokeData.bin", FileMode.Open, FileAccess.Read))
+            {
+                StrokeCollection strokes = new StrokeCollection(fs);
+                this.myInkCanvas.Strokes = strokes;
+            }
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            // Clear all strokes.
+            this.myInkCanvas.Strokes.Clear();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("StrokeData.bin", FileMode.Create))
+            {
+                this.myInkCanvas.Strokes.Save(fs);
+                fs.Close();
+            }
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("StrokeData.bin", FileMode.Open, FileAccess.Read))
+            {
+                StrokeCollection strokes = new StrokeCollection(fs);
+                this.myInkCanvas.Strokes = strokes;
+            }
+        }
+        private void PopulateDocument()
+        {
+            this.listOfFunFacts.FontSize = 12;
+            this.listOfFunFacts.MarkerStyle = TextMarkerStyle.Circle;
+            this.listOfFunFacts.ListItems.Add(new ListItem(new Paragraph(new Run("Fixed documents are for WYSIWYG print ready docs!"))));
+            this.listOfFunFacts.ListItems.Add(new ListItem(new Paragraph(new Run("The API supports tables and embedded figures!"))));
+            this.listOfFunFacts.ListItems.Add(new ListItem(new Paragraph(new Run("Flow documents are read only!"))));
+            this.listOfFunFacts.ListItems.Add(new ListItem(new Paragraph(new Run("BlockUIContaner allows you to embed WPF controls in the document"))));
+
+            //Add some data to the Paragraph
+            //First part of sentence
+            Run prefix = new Run("This paragraph was generated");
+            //Middle of paragraph
+
+            Bold b = new Bold();
+            Run infix = new Run("dynamically");
+            infix.Foreground = Brushes.Red;
+            infix.FontSize = 30;
+            b.Inlines.Add(infix);
+
+            Run suffix = new Run("at runtime! ");
+
+            this.paraBodyText.Inlines.Add(prefix);
+            this.paraBodyText.Inlines.Add(infix);
+            this.paraBodyText.Inlines.Add(suffix);
+        }
+
+        
+
+        
+
+       
+
+    }
+}
